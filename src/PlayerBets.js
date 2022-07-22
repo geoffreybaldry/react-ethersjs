b8e72bce07e59b5d590fa3a8b696b0cd89e4106a
geoff.baldry1 <geoff.baldry1@ihsmarkit.com>
2022-07-22 10:43:18 +0100

    adding playerbets table

---
+++ src/PlayerBets.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const PlayerBets = ( { contract }) => {
+
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    PlayerBets
+                </Col>
+            </Row>
+        </Container>
+    )
+    
+}
+
+export default PlayerBets
